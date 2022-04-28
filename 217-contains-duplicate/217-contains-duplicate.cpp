43b0aed7b20f82144af0603b79f149afde51fa52
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-04-28 09:06:49 +0530

    Time: 23 ms (22.68%), Space: 69.9 MB (37.99%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i;
-        bool dhiraj = false;
-        for(i=1;i<nums.size();i++){
-            if(nums[i]==nums[i-1]){
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        Boolean dhiraj = false;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])
+            {
                 dhiraj = true;
                 break;
             }
         }
-      return dhiraj;  
+        return dhiraj;
     }
-};
+}
