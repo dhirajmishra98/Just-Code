fbc1b85edb7c0a438c28b6d3c7e033ea841d4056
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-05-07 21:57:34 +0530

    Time: 15 ms (51.33%), Space: 12.8 MB (28.97%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int mostFrequent(vector<int>& nums, int key) {
         unordered_map<int,int> m;
+        
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==key)
                 m[nums[i+1]]++;
         }
         
         int freqCount=0;
-        int dhiraj;
+        int ans;
         for(auto &temp : m){
             if(temp.second > freqCount){
                 freqCount = temp.second;
-                dhiraj = temp.first;
+                ans = temp.first;
             }
         }
-        return dhiraj;
+        return ans;
         
         
     }
