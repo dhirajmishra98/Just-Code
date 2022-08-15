78873617218380a778b550cc2b3b2c9fe2a49d9e
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-08-15 12:48:57 +0530

    Time: 6 ms (42.33%), Space: 42.8 MB (54.42%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        ArrayList<String> ans = solve("",digits);
+        return ans;
+    }
+    
+     static ArrayList<String> solve(String empty, String str) {
+        if (str.isEmpty()) {
+            ArrayList<String> arr = new ArrayList<String>();
+            if (empty.isEmpty())
+                return arr;
+
+            arr.add(empty);
+            return arr;
+        }
+
+        ArrayList<String> dhiraj = new ArrayList<>();
+        int digit = str.charAt(0) - '0';
+        if (digit <= 6 && digit >= 2) {
+            for (int i = (digit - 2) * 3; i < (digit - 1) * 3; i++) {
+                char ch = (char) ('a' + i);
+                dhiraj.addAll(solve(empty + ch, str.substring(1)));
+            }
+        } else if (digit == 8) {
+            for (int i = ((digit - 2) * 3) + 1; i < ((digit - 1) * 3) + 1; i++) {
+                char ch = (char) ('a' + i);
+                dhiraj.addAll(solve(empty + ch, str.substring(1)));
+            }
+        } else if (digit == 7) {
+            for (int i = (digit - 2) * 3; i < ((digit - 1) * 3) + 1; i++) {
+                char ch = (char) ('a' + i);
+                dhiraj.addAll(solve(empty + ch, str.substring(1)));
+            }
+        } else if (digit == 9) {
+            for (int i = ((digit - 2) * 3) + 1; i <= ((digit - 1) * 3) + 1; i++) {
+                char ch = (char) ('a' + i);
+                dhiraj.addAll(solve(empty + ch, str.substring(1)));
+            }
+        } else {
+            return dhiraj;
+        }
+        return dhiraj;
+    }
+}
