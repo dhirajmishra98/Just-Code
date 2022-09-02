fd1074d274e03655895238fa974f4515bd5a021b
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-02 07:06:34 +0530

    Time: 4 ms (41.67%), Space: 48.8 MB (53.26%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    class Pair{
+        int soldiers;
+        int index;
+        Pair(int s, int i){
+            this.soldiers = s;
+            this.index = i;
+        }
+    }
+    
+    public int[] kWeakestRows(int[][] mat, int k) {
+        Pair []arr = new Pair[mat.length];
+        for(int i=0;i<mat.length;i++){
+            int sum = 0;
+            for(int j=0;j<mat[i].length;j++){
+                if(mat[i][j]==1){
+                    sum++;
+                } else {
+                    break;
+                }
+            }
+            arr[i] = new Pair(sum,i);
+        }
+        
+        //sorting pair according to less soldiers->more soldiers(ascending)
+        Arrays.sort(arr, (a,b)->a.soldiers-b.soldiers);
+        
+        int[] result = new int[k];
+        for(int i=0;i<k;i++){
+            result[i] = arr[i].index;
+        }
+        return result;
+    }
+}
