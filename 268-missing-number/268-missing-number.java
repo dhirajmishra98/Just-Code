b0cfcb7f4952f12e287a38d11a1f4c109bb17525
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-03 16:20:16 +0530

    Time: 19 ms (5.14%), Space: 50.6 MB (78.29%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,3 +1,49 @@
+
+//Approach 3 (Binary Search)
+class Solution {
+    public int missingNumber(int[] nums) {
+       Arrays.sort(nums);
+        int start = 0;
+        int end = nums.length-1;
+        int target = 0;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(target == nums[mid]){
+                target++;
+                start = 0;
+                end = nums.length-1;
+            }
+            
+            if(target > nums[mid]){
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        }
+        return start;
+    }
+}
+
+
+
+/*
+//Approach 1 
+class Solution {
+    public int missingNumber(int[] nums) {
+       int size = nums.length;
+        int totalSum = (size * (size+1))/2;
+        
+        for(int x : nums){
+            totalSum -= x;
+        }
+        
+        return totalSum ;
+    }
+}
+*/
+
+/*
 //Approach 2 (XOR)
 class Solution {
     public int missingNumber(int[] nums) {
@@ -13,6 +59,7 @@ class Solution {
         return sum;
     }
 }
+*/
 
 
 
