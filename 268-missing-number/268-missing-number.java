f7c0d23d67f47b17d274569117d4edc8476c2111
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-03 16:12:47 +0530

    Time: 1 ms (78.35%), Space: 49.1 MB (85.58%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,3 +1,23 @@
+//Approach 2 (XOR)
+class Solution {
+    public int missingNumber(int[] nums) {
+       int sum = nums[0];
+        for(int i=1;i<nums.length;i++){
+            sum = sum^nums[i];
+        }
+        
+        for(int i=0;i<=nums.length;i++){
+            sum = sum^i;
+        }
+        
+        return sum;
+    }
+}
+
+
+
+/*
+//Approach 1 
 class Solution {
     public int missingNumber(int[] nums) {
        int size = nums.length;
@@ -10,3 +30,4 @@ class Solution {
         return totalSum ;
     }
 }
+*/
