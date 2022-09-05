d66c2197e2e7cc1613f7673ca2f11a7ec38352f0
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-05 22:06:05 +0530

    Time: 1 ms (91.51%), Space: 45.6 MB (76.71%) - LeetHub

--- 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
+++ 589-n-ary-tree-preorder-traversal/589-n-ary-tree-preorder-traversal.java
@@ -17,6 +17,22 @@ class Node {
 };
 */
 
+
+class Solution {
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        if(root==null) return ans;
+        
+        ans.add(root.val);
+        for(Node child : root.children){
+            preorder(child);
+        }
+        return ans;
+    }
+}
+
+/*
+//iterative method
 class Solution {
     public List<Integer> preorder(Node root) {
         List<Integer> ans = new ArrayList<>();
@@ -35,5 +51,5 @@ class Solution {
         }
         return ans;
     }
-    
 }
+*/
