7dd084356ad95e80048491f621ae0d3196ceb357
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-05 22:34:38 +0530

    Time: 7 ms (10.75%), Space: 46.2 MB (49.76%) - LeetHub

--- 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
+++ 590-n-ary-tree-postorder-traversal/590-n-ary-tree-postorder-traversal.java
@@ -18,6 +18,29 @@ class Node {
 */
 
 class Solution {
+    public List<Integer> postorder(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(root==null) return ans;
+        
+        Stack<Node> s = new Stack<>();
+        s.push(root);
+        
+        while(s.size()>0){
+            Node curr = s.pop();
+            ans.add(curr.val);
+            
+            for(Node child : curr.children){
+                s.push(child);
+            }
+        }
+         Collections.reverse(ans);
+        return ans;
+    }
+}
+
+/*
+//recursive
+class Solution {
     ArrayList<Integer> ans = new ArrayList<>();
     public List<Integer> postorder(Node root) {
         if(root==null) return ans;
@@ -31,3 +54,4 @@ class Solution {
         return ans;
     }
 }
+*/
