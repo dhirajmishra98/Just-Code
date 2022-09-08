836c087296f712fff7f2d1962a12ee8c5ee7c26e
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-09-08 21:58:33 +0530

    Time: 18 ms (27.75%), Space: 46.6 MB (12.37%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String str1 = Integer.toString(x);
+        String str2 = new StringBuilder().append(x).reverse().toString();
+        
+        if(str1.equals(str2))
+            return true;
+        
+        return false;
+    }
+}
