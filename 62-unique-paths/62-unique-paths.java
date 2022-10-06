bdf9c8d9bdb3d3161b1a0becebcfd935dd1a8a96
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-06 11:28:23 +0530

    Time: 3 ms (7.04%), Space: 41.4 MB (15.08%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -4,7 +4,19 @@ class Solution {
         for(int []row : dp){
             Arrays.fill(row,-1);
         }
-        return memoise(m-1,n-1,dp);
+        // return memoise(m-1,n-1,dp); //memoization
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j] = 1;
+                else {
+                    int up =0, left =0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
     
     private static int memoise(int i, int j,int [][]dp){
