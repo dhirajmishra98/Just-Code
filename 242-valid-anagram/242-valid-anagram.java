33ca65731c915f3eca27ad797a4e7e5766ea5dfe
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-07 09:04:34 +0530

    Time: 26 ms (22.81%), Space: 44 MB (50.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
         //cannot use hashmap or hashset but using hashtable
-        int[] check= new int[256];
+        int[] check= new int[26];
         for(int i=0;i<s.length();i++){
             check[s.charAt(i)-'a']++;
             check[t.charAt(i)-'a']--;
