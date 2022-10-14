9badf53e7e3abe0a9c350ff002a86648fb173036
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-14 12:33:54 +0530

    Time: 11 ms (21.61%), Space: 207.3 MB (74.46%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -9,28 +9,41 @@
  * }
  */
 class Solution {
-    public ListNode deleteMiddle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode temp = head;
+//     public ListNode deleteMiddle(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         ListNode temp = head;
 
-        if(temp.next == null) return null;
-        if(slow.next.next == null){
-            slow.next = null;
-            return head;
-        }
-        while(fast!=null && fast.next!=null){
-            temp = slow;
-            slow = slow.next;
+//         if(temp.next == null) return null;
+//         if(slow.next.next == null){
+//             slow.next = null;
+//             return head;
+//         }
+//         while(fast!=null && fast.next!=null){
+//             temp = slow;
+//             slow = slow.next;
             
-            if(fast.next.next!=null)
-                fast = fast.next.next;
-            else 
-                break;
-        }
+//             if(fast.next.next!=null)
+//                 fast = fast.next.next;
+//             else 
+//                 break;
+//         }
         
-        temp.next = slow.next;
-        return head;
+//         temp.next = slow.next;
+//         return head;
+//     }
+    
+     public ListNode deleteMiddle(ListNode head) {
+         if(head.next == null) return null;
+        ListNode slow = head;
+         ListNode fast = head.next.next;
+         
+         while(fast!=null && fast.next != null){
+             fast = fast.next.next;
+             slow = slow.next;
+         }
+         slow.next = slow.next.next;
+         return head;
     }
     
 }
