92dcca26c4874530c3c6af4bd621ef53290461c4
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-18 00:12:54 +0530

    Time: 15 ms (37.26%), Space: 41.2 MB (27.18%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,12 +1,24 @@
 public class Solution {
     public boolean judgeSquareSum(int c) {
+        //Two pointers approach
+        long i=0,j=(long)Math.sqrt(c);
+        while(i<=j){
+            long curr = i*i + j*j;
+            if(curr < c) i++;
+            else if(curr > c) j--;
+            else return true;
+        }
+        return false;
         
+        /*
+        //Binary search
         for(long i=0;i*i <= c; i++){
             long b = c-i*i;
             boolean check = binarySearch(0,b,b);
             if(check) return true;
         }
         return false;
+        */
         
         /*
         //optimized Brute force 2
