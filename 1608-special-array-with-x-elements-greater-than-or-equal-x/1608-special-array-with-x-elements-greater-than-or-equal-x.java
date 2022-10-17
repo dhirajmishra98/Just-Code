3e0a30eee7e1d12bbc34e3525b1ef6151eeb3f57
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-17 17:19:53 +0530

    Time: 1 ms (92.07%), Space: 41.7 MB (56.82%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -1,9 +1,35 @@
 class Solution {
     public int specialArray(int[] nums) {
-       int count = 0,i;
+        // return bruteForce(nums);
+        return binarySearch(nums);
+    }
+    
+    private static int binarySearch(int[] arr){
+        int start = 0, end = arr.length;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            int curr = count(arr,mid);
+            
+            if(mid == curr) return mid;
+            if(curr > mid) start = mid+1;
+            else end = mid-1;
+        }
+        return -1;
+    }
+    
+    private static int count(int[] arr, int target){
+        int count = 0;
+        for(int x : arr){
+            if(x>=target) count++;
+        }
+        return count;
+    }
+    
+    private static int bruteForce(int[] nums){
+        int count = 0,i,j;
         for(i=1;i<=nums.length;i++){
             count = 0;
-            for(int j=0;j<nums.length;j++){
+            for(j=0;j<nums.length;j++){
                 if(nums[j] >= i) count++;
             }
             if(count == i) return count;
