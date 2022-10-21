290ee0907703d9e906ce0992b651443cdc556cfe
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-21 16:49:48 +0530

    Time: 13 ms (11.05%), Space: 49.7 MB (71.71%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        //Binary Search : TC=O(logN), SC=O(1)
         int i=0,j=numbers.length-1;
         while(i<j){
             if((numbers[i]+numbers[j] )== target ){
@@ -12,6 +13,17 @@ class Solution {
                 j--;
             }
         }
+        return null; 
+        
+        /*
+        //BruteForce : TC = (N*N), SC=O(1) -> TLE
+            for(int i=0;i<numbers.length;i++){
+                for(int j=i+1;j<numbers.length;j++){
+                    if(numbers[i]+numbers[j]==target) return new int[]{i+1,j+1};
+                }
+            }
         return null;
+        */
+        
     }
 }
