1e693d40674e38ddb88f9a2bbcdf77043010d340
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-10-31 23:12:48 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (77.10%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int climbStairs(int n) {
+        // return count(n);  //TLE
+        
+        int []dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return dptable(n,dp);
+        
+    }
+    
+    private int dptable (int n, int[] dp){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        if(dp[n] > 0) return dp[n];
+        // else {
+        //     int left = dptable(n-1,dp);
+        //     int right = dptable(n-2,dp);
+        //     dp[n] = left+right;
+        // }
+        
+        return dp[n] = dptable(n-1,dp)+dptable(n-2,dp);
+    }
+    
+    private int count(int n){
+        if(n==0) return 1;
+        if(n<0) return 0;
+        return climbStairs(n-1)+climbStairs(n-2);
+    }
+}
