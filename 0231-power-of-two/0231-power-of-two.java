aeb6ee21cf0ff63208bb9fae851b9a4da42919a0
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-11-01 15:11:56 +0530

    Time: 2 ms (81.13%), Space: 41.8 MB (12.25%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -6,6 +6,14 @@ class Solution {
         // }
         // return ans==n;
         
-        return n>0 && (n&(n-1)) == 0;
+        // return n>0 && (n&(n-1)) == 0;
+        
+        int count = 0;
+        while(n>0){
+            int temp = n&1;
+            if(temp==1) count++;
+            n = n>>1;
+        }
+        return count==1;
     }
 }
