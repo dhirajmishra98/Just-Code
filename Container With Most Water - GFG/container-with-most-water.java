9855dea29b1d88e19ea1b33d24ae29e257f01e58
Dhiraj Mishra <gobindmishra23@gmail.com>
2022-12-27 11:10:34 +0545

    Added solution - LeetHub

---
+++ Container With Most Water - GFG/container-with-most-water.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    int n = Integer.parseInt(br.readLine()); // input size of array
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split("\\s+");
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]); // input elements of array
+		    }
+		    Solve T = new Solve();
+            long ans = T.maxArea(arr,n);
+            System.out.println(ans);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solve{
+    
+    long maxArea(int A[], int len){
+        if(len <= 1) return 0;
+        
+        /*
+        //BruteForce : TC=O(n2), SC=O(1)
+        long ans = Integer.MIN_VALUE;
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len;j++){
+                ans = Math.max(ans,Math.min(A[i],A[j])*Math.abs(i-j));
+            }
+        }
+        return ans;
+        */
+        
+        int i=0,j=len-1;
+        long ans = Integer.MIN_VALUE;
+        while(i<j){
+            ans = Math.max(ans,Math.min(A[i],A[j])*Math.abs(i-j));
+            if(A[i]<=A[j]) i++;
+            else j--;
+        }
+        return ans;
+    }
+    
+}
