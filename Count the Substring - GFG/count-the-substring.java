e3030e64199c821b29b4af5a5b1e014720c15af0
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-01-15 17:22:00 +0545

    Added solution - LeetHub

--- Count the Substring - GFG/count-the-substring.java
+++ Count the Substring - GFG/count-the-substring.java
@@ -58,10 +58,10 @@ class GFG {
 
 class Solution {
     long countSubstring(String S){
-        int n = S.length();
+       int n = S.length();
         long ans = 0, minus = 0;
         int zero = n;
-        int dp[] = new int[2 * n + 5];
+        int mp[] = new int[2 * n + 5];
         int cur=zero;
         for(int i = 0; i < n; i++){
             if(S.charAt(i) == '0')
@@ -71,20 +71,20 @@ class Solution {
             if(cur <= zero){
                 minus++;
             }
-            dp[cur]++;
+            mp[cur]++;
         }
         for(int i = 0; i < n; i++){
             ans += (n - i - minus) * 1l;
             if(S.charAt(i) == '1'){
-                dp[zero+1]--;
+                mp[zero+1]--;
                 zero++;
-                minus += dp[zero];
+                minus += mp[zero];
             }
             else{
-                dp[zero-1]--;
+                mp[zero-1]--;
                 zero--;
                 minus--;
-                minus -= dp[zero+1];
+                minus -= mp[zero+1];
             }
         }
         return ans;
