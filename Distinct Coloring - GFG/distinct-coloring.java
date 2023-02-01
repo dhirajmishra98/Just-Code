0d9ffa3b7fb87c288f5e9906b3ec3f5150719a78
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-02-01 16:29:56 +0545

    Added solution - LeetHub

--- Distinct Coloring - GFG/distinct-coloring.java
+++ Distinct Coloring - GFG/distinct-coloring.java
@@ -10,30 +10,6 @@ import java.util.*;
 
 class Solution{
     public static long distinctColoring(int N, int[]r, int[]g, int[]b){
-        /*
-        //TC=O(N*3), SC=O(N*3)
-        long [][]cost = new long[N][3];
-        cost[0][0] = r[0]; cost[0][1] = g[0]; cost[0][2] = b[0];
-        
-        for(int i=1;i<N;i++){
-            for(int j=0;j<3;j++){
-                if (j==0){
-                    cost[i][j] = r[i];
-                    cost[i][j] += Math.min(cost[i-1][j+1],cost[i-1][j+2]);
-                } else if (j==1){
-                    cost[i][j] = g[i];
-                    cost[i][j] += Math.min(cost[i-1][j-1],cost[i-1][j+1]);
-                } else {
-                    cost[i][j] = b[i];
-                    cost[i][j] += Math.min(cost[i-1][j-2],cost[i-1][j-1]);
-                }
-            }
-        }
-        
-        return Math.min(Math.min(cost[N-1][0],cost[N-1][1]),cost[N-1][2]);
-        */
-        
-        //TC=O(N*3), SC=O(3)
         long []cost = new long[3];
         cost[0] = r[0]; cost[1] = g[0]; cost[2] = b[0];
         
@@ -57,8 +33,6 @@ class Solution{
         return Math.min(Math.min(cost[0],cost[1]),cost[2]);
     }
 }
-
-
 
 
 //{ Driver Code Starts.
