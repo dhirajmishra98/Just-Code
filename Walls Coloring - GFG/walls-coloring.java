06a6c82bf203ac4065aefab8bfbc458cc0017e22
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-02-14 12:29:04 +0530

    Added solution - LeetHub

--- Walls Coloring - GFG/walls-coloring.java
+++ Walls Coloring - GFG/walls-coloring.java
@@ -32,7 +32,7 @@ class GFG{
 
 class Solution{
 	int minCost(int [][] colors, int N){
-        //Write your code here
+	    /*
         int [][]dp = new int[N][3];
         dp[0][0] = colors[0][0]; dp[0][1] = colors[0][1]; dp[0][2] = colors[0][2];
         
@@ -50,6 +50,27 @@ class Solution{
         
         // System.out.println(Arrays.deepToString(dp));
         return Math.min(dp[N-1][0],Math.min(dp[N-1][1],dp[N-1][2]));
+        */
+        
+        int []dp = new int[3];
+        dp[0] = colors[0][0]; dp[1] = colors[0][1]; dp[2] = colors[0][2];
+        
+        for(int i=1;i<N;i++){
+            int []temp = new int[3];
+            for(int j=0;j<3;j++){
+                if(j==0){
+                    temp[j] = colors[i][j] + Math.min(dp[1],dp[2]);
+                }else if(j==1){
+                    temp[j] = colors[i][j] + Math.min(dp[0],dp[2]);
+                } else {
+                    temp[j] = colors[i][j] + Math.min(dp[0],dp[1]);
+                }
+            }
+            dp = temp.clone();
+        }
+        
+        // System.out.println(Arrays.toString(dp));
+        return Math.min(dp[0],Math.min(dp[1],dp[2]));
         
     }
 }
