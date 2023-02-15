4a5257484cae8ee3ea1e50582301422e1ed5ba8d
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-02-16 00:00:05 +0530

    Added solution - LeetHub

--- Geeks Island - GFG/geeks-island.java
+++ Geeks Island - GFG/geeks-island.java
@@ -33,47 +33,58 @@ class GFG{
 //User function Template for Java
 
 class Solution{
-	private void dfs(int[][] mat,int i,int j,int n,int m,boolean[][] visited){
-        if(i<0 || i>=n || j<0 || j>=m || visited[i][j])
-            return;
-        visited[i][j] = true;
-        
-        if(j+1 < m && mat[i][j+1] >= mat[i][j] && !visited[i][j+1]) // right
-            dfs(mat,i,j+1,n,m,visited);
-        if(i+1 < n && mat[i+1][j] >= mat[i][j] && !visited[i+1][j]) // down
-            dfs(mat,i+1,j,n,m,visited);
-        if(j-1 >= 0 && mat[i][j-1] >= mat[i][j] && !visited[i][j-1]) //left
-            dfs(mat,i,j-1,n,m,visited);
-        if(i-1 >= 0 && mat[i-1][j] >= mat[i][j] && !visited[i-1][j])// up
-            dfs(mat,i-1,j,n,m,visited);
-    }
-    
 	int water_flow(int [][] mat, int n, int m) {
-		boolean[][] indianOcean = new boolean[n][m];
-		boolean[][] arabianSea = new boolean[n][m];
+		boolean [][]indianOcean = new boolean[n][m];
+		boolean [][]arabianSea = new boolean[n][m];
 		
-		/** Indian ocean */
-		for(int j=0;j<m;j++)
-		    dfs(mat,0,j,n,m,indianOcean);
-
-		for(int i=0;i<n;i++)
+		//for indianOcean we can enter from 0th row and 0th col
+		for(int i=0;i<n;i++){ //for N row from indian ocean
 		    dfs(mat,i,0,n,m,indianOcean);
+		}
 		
-		/** Arabian Sea */
-		for(int j=0;j<m;j++)
-		    dfs(mat,n-1,j,n,m,arabianSea);
+		for(int i=0;i<m;i++){ //for M col from indian ocean
+		    dfs(mat,0,i,n,m,indianOcean);
+		}
 		
-		for(int i=0;i<n;i++)
+		//for arabianOcean we can enter from n-1 row and m-1 col
+		for(int i=0;i<m;i++){ //moving from down row for M cols
+		    dfs(mat,n-1,i,n,m,arabianSea);
+		}
+		
+		for(int i=0;i<n;i++){ //moving from right side for n rows
 		    dfs(mat,i,m-1,n,m,arabianSea);
+		}
 		
-		
-		int count = 0;
+		//check how many island can reach both ocean
+		int validIslands = 0;
 		for(int i=0;i<n;i++){
 		    for(int j=0;j<m;j++){
 		        if(indianOcean[i][j] && arabianSea[i][j])
-		            count++;
+		            validIslands++;
 		    }
 		}
-		return count;
+		
+		return validIslands;
+	}
+	
+	private void dfs(int[][] mat, int i, int j, int n, int m, boolean[][] visited){
+	    if(i<0 || i>=n || j<0 || j>=m || visited[i][j]) return;
+	    visited[i][j] = true;
+	    
+	    if(i-1>=0 && !visited[i-1][j] && mat[i-1][j]>=mat[i][j]){ //moving up
+	        dfs(mat,i-1,j,n,m,visited);
+	    }
+	    
+	    if(i+1<n && !visited[i+1][j] && mat[i+1][j]>=mat[i][j]){ //moving down
+	        dfs(mat,i+1,j,n,m,visited);
+	    }
+	    
+	    if(j-1>=0 && !visited[i][j-1] && mat[i][j-1]>=mat[i][j]){ //moving left
+	        dfs(mat,i,j-1,n,m,visited);
+	    }
+	    
+	    if(j+1<m && !visited[i][j+1] && mat[i][j+1]>=mat[i][j]){ //moving right
+	        dfs(mat,i,j+1,n,m,visited);
+	    }
 	}
 }
