6456defc3e052ebccfb73d1adf21d5cafba7a9c2
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-03-10 15:22:58 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.java
+++ Maximum Triplet product - GFG/maximum-triplet-product.java
@@ -13,21 +13,35 @@ import java.util.*;
 class Solution {
     Long maxTripletProduct(Long arr[], int n)
     {
-        // long min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-        // long max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+        long min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        long max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
         
-        // for(int i=0;i<n;i++){
-        //     if(min1 > arr[i]){
-        //         min1 = arr[i];
-        //     }else if(min1<arr[i] && min2>arr[i]){
-        //         min2 = arr[i];
-        //     }
+        for(int i=0;i<n;i++){
+            if(arr[i] > max1){
+                max3 = max2;
+                max2 = max1;
+                max1 = arr[i];
+            }else if(arr[i] > max2){
+                max3 = max2;
+                max2 = arr[i];
+            }else if(arr[i]> max3){
+                max3 = arr[i];
+            }
             
-            
-        // }
+            if(arr[i] < min1){
+                min2 = min1;
+                min1 = arr[i];
+            }else if(arr[i] < min2){
+                min2 = arr[i];
+            }
+        }
         
-        Arrays.sort(arr);
-        return Math.max(arr[0]*arr[1]*arr[n-1], arr[n-1]*arr[n-2]*arr[n-3]);
+        // System.out.println(min1+" "+min2+" "+max1+" "+max2+" "+max3);
+        return Math.max(min1*min2*max1, max1*max2*max3);
+        
+        //TC = O(nlogn)
+        // Arrays.sort(arr);
+        // return Math.max(arr[0]*arr[1]*arr[n-1], arr[n-1]*arr[n-2]*arr[n-3]);
     }
 }
 
