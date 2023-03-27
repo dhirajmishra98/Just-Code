77d75b3ed39c268974f911e2cdaa3c92471554f5
Dhiraj Mishra <gobindmishra23@gmail.com>
2023-03-27 21:50:32 +0530

    Added solution - LeetHub

---
+++ Divide in Incremental Groups - GFG/divide-in-incremental-groups.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+
+            int N = Integer.parseInt(br.readLine().trim());
+            int K = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            System.out.println(ob.countWaystoDivide(N, K));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int countWaystoDivide(int N, int K) {
+        int [][][] dp = new int[N+1][N+1][K+1];
+        for(int [][]x : dp){
+            for(int []y : x){
+                Arrays.fill(y,-1);
+            }
+        }
+        
+        return solve(N,K,1,dp);
+    }
+    
+    private int solve(int n, int k, int prev,int [][][]dp){
+        if(k==0){
+            if(n==0) return 1;
+            return 0;
+        }
+        if(n==0) return 0;
+        if(dp[n][prev][k] != -1) return dp[n][prev][k];
+        
+        int count = 0;
+        for(int i=prev;i<=n;i++){
+            count += solve(n-i,k-1,i,dp);
+        }
+        
+        return dp[n][prev][k] = count;
+    }
+}
